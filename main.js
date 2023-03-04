89128a92717172b4001365a2be4d2f32d1f3080f
Szuniga29 <stevenza.129@gmail.com>
2023-03-04 15:14:15 -0600

    menu lateral

--- main.js
+++ main.js
@@ -39,13 +39,12 @@ function volverArriba() {
 document.documentElement.scrollTop = 0;
 }
 
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "images/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "images/close_white_36dp.svg";
-    }
-}
+function hideMenu() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('show');
+  }
+
+  function showMenu() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.add('show');
+  }
